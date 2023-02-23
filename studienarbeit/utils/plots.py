309f11cb7767c36893a9b033786f6db175d8c6bf
Felix Hoffmann <felix.emmanuel.hoffmann@gmail.com>
2023-02-23 08:31:17 +0100

    🚧 Move plots and update sentiment

--- studienarbeit/utils/plots.py
+++ studienarbeit/utils/plots.py
@@ -1,50 +1,54 @@
+import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-sns.set(style="white", palette="muted", rc={"figure.figsize": (20, 8)})
-line_kws = {"color": "r", "alpha": 0.7, "lw": 5}
 
+class Plot:
+    def __init__(self):
+        self.color_palette = "muted"
+        sns.set(style="white", palette=self.color_palette, rc={"figure.figsize": (20, 8)})
+        self.line_kws = {"color": "r", "alpha": 0.7, "lw": 5}
 
-def plot_party_count(df, column="party", title="Parteien"):
-    fig, axs = plt.subplots()
+    def plot_party_count(self, df: pd.DataFrame, column="party", title="Parteien"):
+        fig, axs = plt.subplots()
 
-    sns.countplot(x=column, data=df, ax=axs)
+        sns.countplot(x=column, data=df, ax=axs)
 
-    fig.suptitle(title)
-    axs.set_xlabel("Parteien")
-    axs.set_ylabel("Anzahl")
+        fig.suptitle(title)
+        axs.set_xlabel("Parteien")
+        axs.set_ylabel("Anzahl")
 
+    def plot_sentiment(self, df: pd.DataFrame, column="sentiment", title="Sentiment"):
+        fig, axs = plt.subplots(1, 2)
 
-def plot_sentiment(df, column="sentiment", title="Sentiment"):
-    fig, axs = plt.subplots()
+        sns.kdeplot(data=df, x=column, hue="party", palette=self.color_palette, ax=axs[0])
+        sns.boxenplot(data=df, x="party", y=column, ax=axs[1])
 
-    sns.kdeplot(data=df, x=column, hue="party", ax=axs)
+        fig.suptitle(title)
+        axs[0].set_xlabel("Sentiment")
+        axs[0].set_ylabel("Anzahl")
+        axs[1].set_xlabel("Parteien")
+        axs[1].set_ylabel("Sentiment")
 
-    fig.suptitle(title)
-    axs.set_xlabel("Parteien")
-    axs.set_ylabel("Anzahl")
+    def plot_word_count(self, df: pd.DataFrame, column="word_count", title="Wortanzahl", x_lim=100):
+        fig, axs = plt.subplots(1, 2)
 
+        sns.kdeplot(data=df, x=column, hue="party", palette=self.color_palette, ax=axs[0])
+        sns.boxenplot(data=df, x="party", y=column, ax=axs[1])
 
-def plot_word_count(df, column="word_count", title="Wortanzahl", x_lim=100):
-    fig, axs = plt.subplots(1, 2)
+        fig.suptitle(title)
+        axs[0].set_xlabel("Wortanzahl")
+        axs[0].set_ylabel("Anzahl")
+        axs[0].set_xlim(0, x_lim)
+        axs[1].set_xlabel("Parteien")
+        axs[1].set_ylabel("Wortanzahl")
 
-    sns.kdeplot(data=df, x=column, hue="party", ax=axs[0])
-    sns.boxenplot(data=df, x="party", y=column, ax=axs[1])
+    def plot_gender(self, df: pd.DataFrame, column="gender", title="Geschlechterverteilung pro Partei"):
+        _, axs = plt.subplots()
 
-    fig.suptitle(title)
-    axs[0].set_xlabel("Wortanzahl")
-    axs[0].set_ylabel("Anzahl")
-    axs[0].set_xlim(0, x_lim)
-    axs[1].set_xlabel("Parteien")
-    axs[1].set_ylabel("Wortanzahl")
+        sns.countplot(data=df, x="party", hue=column, ax=axs)
 
-
-def plot_gender(df, column="gender", title="Geschlechterverteilung pro Partei"):
-    _, axs = plt.subplots()
-
-    sns.countplot(data=df, x="party", hue=column, ax=axs)
-
-    axs.set_title(title)
-    axs.set_xlabel("Parteien")
-    axs.set_ylabel("Anzahl")
-    axs.legend(title="Geschlecht")
+        fig.suptitle(title)
+        axs.set_xlabel("Parteien")
+        axs.set_ylabel("Anzahl")
+        axs.legend(title="Geschlecht")
